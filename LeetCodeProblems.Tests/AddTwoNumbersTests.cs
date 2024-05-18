e4172f4d1969e4a7f301b7cf8b87caa88681ac0a
Behnam Salehi <behnam.dev@outlook.com>
2024-05-18 22:04:58 +0330

    Update AddTwoNumbersTests.cs

--- LeetCodeProblems.Tests/AddTwoNumbersTests.cs
+++ LeetCodeProblems.Tests/AddTwoNumbersTests.cs
@@ -2,49 +2,21 @@ namespace LeetCodeProblems.Tests;
 
 public class AddTwoNumbersTests
 {
-    private readonly AddTwoNumbers sut;
+    private readonly AddTwoNumbers sut = new();
 
-    public AddTwoNumbersTests()
-    {
-        sut = new();
-    }
-
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("243", "564", "708")]
+    [InlineData("0", "0", "0")]
+    [InlineData("9999999", "9999", "89990001")]
+    public void Test_Pass(string number1, string number2, string expected)
     {
         // Given
-        var l1 = Utils.StringToListNode("243");
-        var l2 = Utils.StringToListNode("564");
+        var listNode1 = Utils.StringToListNode(number1);
+        var listNode2 = Utils.StringToListNode(number2);
         // When
-        var result = sut.Solution(l1, l2);
-        var expected = "708";
+        var result = sut.Solution(listNode1, listNode2);
         // Then
-        Assert.Equal(expected, Utils.ListNodeToString(result));
-    }
-
-    [Fact]
-    public void Test2()
-    {
-        // Given
-        var l1 = Utils.StringToListNode("0");
-        var l2 = Utils.StringToListNode("0");
-        // When
-        var result = sut.Solution(l1, l2);
-        var expected = "0";
-        // Then
-        Assert.Equal(expected, Utils.ListNodeToString(result));
-    }
-
-    [Fact]
-    public void Test3()
-    {
-        // Given
-        var l1 = Utils.StringToListNode("9999999");
-        var l2 = Utils.StringToListNode("9999");
-        // When
-        var result = sut.Solution(l1, l2);
-        var expected = "89990001";
-        // Then
-        Assert.Equal(expected, Utils.ListNodeToString(result));
+        var actual = Utils.ListNodeToString(result);
+        Assert.Equal(expected, actual);
     }
 }
