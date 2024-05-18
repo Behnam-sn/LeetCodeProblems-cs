6d3c406beb801add253827539e151b5074455624
Behnam Salehi <behnam.dev@outlook.com>
2024-05-18 21:47:03 +0330

    Valid Sudoku

---
+++ LeetCodeProblems.Domain/ValidSudoku.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCodeProblems.Domain;
+
+public class ValidSudoku
+{
+    public bool Solution(char[][] board)
+    {
+        for (int x = 0; x < 9; x++)
+        {
+            var hashSet = new HashSet<char>();
+
+            for (int y = 0; y < 9; y++)
+            {
+                var cell = board[x][y];
+
+                if (cell == '.')
+                {
+                    continue;
+                }
+
+                if (hashSet.Contains(cell) == true)
+                {
+                    return false;
+                }
+
+                hashSet.Add(cell);
+            }
+        }
+
+        for (int y = 0; y < 9; y++)
+        {
+            var hashSet = new HashSet<char>();
+
+            for (int x = 0; x < 9; x++)
+            {
+                var cell = board[y][x];
+
+                if (cell == '.')
+                {
+                    continue;
+                }
+
+                if (hashSet.Contains(cell) == true)
+                {
+                    return false;
+                }
+
+                hashSet.Add(cell);
+            }
+        }
+
+        return true;
+    }
+}
---
+++ LeetCodeProblems.Tests/ValidSudokuTests.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeProblems.Tests;
+
+public class ValidSudokuTests
+{
+    private readonly ValidSudoku sut;
+
+    public ValidSudokuTests()
+    {
+        sut = new();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Given
+        var board = new char[][]
+        {
+            new char[] {'5','3','.','.','7','.','.','.','.'},
+            new char[] {'6','.','.','1','9','5','.','.','.'},
+            new char[] {'.','9','8','.','.','.','.','6','.'},
+            new char[] {'8','.','.','.','6','.','.','.','3'},
+            new char[] {'4','.','.','8','.','3','.','.','1'},
+            new char[] {'7','.','.','.','2','.','.','.','6'},
+            new char[] {'.','6','.','.','.','.','2','8','.'},
+            new char[] {'.','.','.','4','1','9','.','.','5'},
+            new char[] {'.','.','.','.','8','.','.','7','9'},
+        };
+        // When
+        var actual = sut.Solution(board);
+        // Then
+        Assert.True(actual);
+    }
+}
