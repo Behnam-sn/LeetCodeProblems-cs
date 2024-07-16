59b89e5efdb1a11d91c50b3204f82b37b59589bb
Behnam Salehi <behnam.dev@outlook.com>
2024-07-16 06:42:39 +0330

    Update BestTimeToBuyAndSellStock

--- LeetCodeProblems.Tests/BestTimeToBuyAndSellStockTests.cs
+++ LeetCodeProblems.Tests/BestTimeToBuyAndSellStockTests.cs
@@ -8,11 +8,12 @@ public class BestTimeToBuyAndSellStockTests
     [InlineData(new int[] { 1 }, 0)]
     [InlineData(new int[] { 7, 1, 5, 3, 6, 4 }, 5)]
     [InlineData(new int[] { 7, 6, 4, 3, 1 }, 0)]
+    [InlineData(new int[] { 2, 1, 2, 0, 1 }, 1)]
     public void Test(int[] prices, int expected)
     {
         // Given
         // When
-        var actual = BestTimeToBuyAndSellStock.Solution1(prices);
+        var actual = BestTimeToBuyAndSellStock.Solution2(prices);
         // Then
         Assert.Equal(expected, actual);
     }
--- LeetCodeProblems/BestTimeToBuyAndSellStock.cs
+++ LeetCodeProblems/BestTimeToBuyAndSellStock.cs
@@ -6,13 +6,13 @@ public class BestTimeToBuyAndSellStock
 {
     public static int Solution1(int[] prices)
     {
-        var maxProfit = 0;
 
         if (prices == null || prices.Length < 2)
         {
-            return maxProfit;
+            return 0;
         }
 
+        var maxProfit = 0;
         for (var i = 0; i < prices.Length; i++)
         {
             var item1 = prices[i];
@@ -28,6 +28,36 @@ public class BestTimeToBuyAndSellStock
                         maxProfit = temp;
                     }
                 }
+            }
+        }
+        return maxProfit;
+    }
+
+    public static int Solution2(int[] prices)
+    {
+        if (prices == null || prices.Length < 2)
+        {
+            return 0;
+        }
+
+        var maxProfit = 0;
+        var lowestPrice = prices[0];
+        var highestPrice = prices[0];
+
+        for (var i = 1; i < prices.Length; i++)
+        {
+            var price = prices[i];
+
+            if (price < lowestPrice)
+            {
+                lowestPrice = price;
+                highestPrice = price;
+            }
+
+            else if (price > highestPrice)
+            {
+                highestPrice = price;
+                maxProfit = Math.Max(highestPrice - lowestPrice, maxProfit);
             }
         }
 
