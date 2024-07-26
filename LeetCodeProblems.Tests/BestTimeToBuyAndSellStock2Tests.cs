8555913bdfa45f0858999a572626681446dd90b4
Behnam Salehi <behnam.dev@outlook.com>
2024-07-27 00:30:03 +0330

    Create BestTimeToBuyAndSellStock2

---
+++ LeetCodeProblems.Tests/BestTimeToBuyAndSellStock2Tests.cs
@@ -0,0 +1,18 @@
+namespace LeetCodeProblems.Tests;
+
+public class BestTimeToBuyAndSellStock2Tests
+{
+    [Theory]
+    [InlineData(new int[] { 1 }, 0)]
+    [InlineData(new int[] { 7, 1, 5, 3, 6, 4 }, 7)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4)]
+    [InlineData(new int[] { 7, 6, 4, 3, 1 }, 0)]
+    public void Test(int[] prices, int expected)
+    {
+        // Given
+        // When
+        var actual = BestTimeToBuyAndSellStock2.Solution(prices);
+        // Then
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ LeetCodeProblems/BestTimeToBuyAndSellStock2.cs
@@ -0,0 +1,10 @@
+
+namespace LeetCodeProblems;
+
+public class BestTimeToBuyAndSellStock2
+{
+    public static int Solution(int[] prices)
+    {
+        throw new NotImplementedException();
+    }
+}
