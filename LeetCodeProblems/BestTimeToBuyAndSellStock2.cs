7029ea0e89129cf185c85b1632cd20af323167e2
Behnam Salehi <behnam.dev@outlook.com>
2024-07-27 00:43:24 +0330

    Update BestTimeToBuyAndSellStock2

--- LeetCodeProblems/BestTimeToBuyAndSellStock2.cs
+++ LeetCodeProblems/BestTimeToBuyAndSellStock2.cs
@@ -3,8 +3,25 @@ namespace LeetCodeProblems;
 
 public class BestTimeToBuyAndSellStock2
 {
-    public static int Solution(int[] prices)
+    public static int Solution1(int[] prices)
     {
-        throw new NotImplementedException();
+        if (prices.Length == 1)
+        {
+            return 0;
+        }
+
+        var maxProfit = 0;
+        for (var i = 0; i < prices.Length; i++)
+        {
+            for (var j = 0; j < i; j++)
+            {
+                if (prices[j] < prices[i])
+                {
+                    // add indexes to possibleDeals
+                }
+            }
+        }
+        // find maxProfit in possibleDeals 
+        return maxProfit;
     }
 }
